fd118b693c33ccb5b10e949c97e731f36b193444
Bassma <Bassmacherfi@gmail.com>
2025-07-06 19:31:52 +0200

    Injected the weather data js

--- app.js
+++ app.js
@@ -2,8 +2,40 @@ function refrechWeather(response) {
   let temperatureElement = document.querySelector("#weather-app-current");
   let temperature = response.data.temperature.current;
   let headingOne = document.querySelector("#weather-app-city");
+  let currentWeatherState = document.querySelector("#weather-details");
+  let currentHumidity = document.querySelector("#Humidity");
+  let humidity = `${response.data.temperature.humidity}%`;
+  let windSpeed = document.querySelector("#wind-speed");
+  let speedOfWind = ` ${response.data.wind.speed} km/h`;
+  let weatherInfo = response.data.condition.description;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  currentWeatherState.innerHTML = weatherInfo;
+  currentHumidity.innerHTML = humidity;
   headingOne.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  windSpeed.innerHTML = speedOfWind;
+  timeElement.innerHTML = formattedDate(date);
+}
+
+function formattedDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Thursday",
+    "Wednesday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `${minutes}`;
+  }
+  return `${day}, ${hours}:${minutes}`;
 }
 function searchCity(city) {
   let apiKey = "267ca2c9b55504a4o4ef34d3b037dtb8";
